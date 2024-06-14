3fba4bce47666a69b1468bdddcce88fbe0f9babd
Francisco-Guido <francisconguido@gmail.com>
2024-06-14 16:16:08 -0300

    Delete unused imports

--- src/main/java/com/project/movies/service/impl/DirectorService.java
+++ src/main/java/com/project/movies/service/impl/DirectorService.java
@@ -4,13 +4,10 @@ import com.project.movies.model.Movie;
 import com.project.movies.model.ApiResponse;
 import com.project.movies.service.IDirectorService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
-import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
